195f6ff026f52019ba22872c67c13766db8ac25c
あり <ari@arinoMacBook-Pro.local>
2025-08-06 19:43:04 +0900

    ポート設定を修正

--- app.py
+++ app.py
@@ -485,5 +485,5 @@ def static_files(filename):
 
 if __name__ == '__main__':
     init_database()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=False)
